0d9f4f70ef0f82db8e24e2064d756d374e8b27d6
michał gozdek <michalgozdek12@gmail.com>
2023-02-22 15:17:58 +0100

    posible to add new task

--- js/code.js
+++ js/code.js
@@ -25,7 +25,20 @@
 
   const init = () => {
     render();
+    const form = document.querySelector(".js-form");
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+      if (newTaskContent === "") {
+        return;
+      }
+
+      tasks.push({
+        content: newTaskContent,
+      });
+      render();
+    });
   };
   init();
 }
-fu;
